0aec9d35f02a6f8b518171ffb4be18d740a0936b
Prashanth yejje <pyejje@bitbucket.allmysons.com>
2024-09-11 10:28:52 +0530

    javascript

---
+++ ps.js
@@ -0,0 +1,54 @@
+// let arr=[10,8,6,2,14,3,1] 
+// let emp=[]
+// for(i=0;i<arr.length;i++){
+//     if(i%2==0 && arr[i]%2==0){
+//         emp.push("even")
+//     }else{
+//         emp.push(arr[i])
+//     }
+   
+   
+// }
+// console.log(emp);
+
+
+
+// let rows=5
+// for(i=1;i<=2*rows-1;i++){
+//     s=""
+
+//     space= i<=rows?i-1:2*rows-i-1
+//     cols=  i<=rows?rows-i+1:i-rows+1
+
+//     for(sp=1;sp<=space;sp++){
+//         s=s+" "
+//     }
+//     if(i<=rows){
+//     for(j=1;j<=cols;j++){
+//         if(i==1||j==1||j==cols){
+//             s=s+j+" "
+//         }else{
+//             s=s+" "+" "
+//         }
+//     }
+// }
+//    else {
+//       for (let j = 1; j <= cols; j++) {
+//         if (j == 1 || i==2*rows-1 ||  j == cols) {
+//           s += j+ " ";
+//         } else {
+//           s += "  ";
+//         }
+//       }
+//     }
+
+//     console.log(s)
+// }
+
+
+let str="program"
+sub=""
+mid=Math.floor(str.length/2);
+for(i=mid;i<=str.length;i++){
+    console.log(mid)
+}
